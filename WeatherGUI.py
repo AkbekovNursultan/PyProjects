26f66e9566afae88460f70aeba5c560d75b10101
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2023-12-08 11:03:17 +0600

    get info from site

--- WeatherGUI.py
+++ WeatherGUI.py
@@ -17,3 +17,15 @@ def format_response(weather_json):
         final_str = 'There was a problem retrieving that information'
     #final_str = 'hello'
     return final_str
+def get_weather(city):
+    weather_key = 'edffd1bf975a74d5d10e58c5ac8be2d3'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': 'edffd1bf975a74d5d10e58c5ac8be2d3', 'q': city, 'units':'metric'}
+    response = requests.get(url, params=params)
+    print(response.json())
+    weather_json = response.json()
+
+    results['text'] = format_response(response.json())
+
+    icon_name = weather_json['weather'][0]['icon']
+    open_image(icon_name)
