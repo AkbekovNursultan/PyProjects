63d6e63de624a63d89fe63b8af2984005eb25c3a
nlls-05 <138480588+nlls-05@users.noreply.github.com>
2023-12-08 12:01:20 +0600

    frame placement

--- WeatherGUI.py
+++ WeatherGUI.py
@@ -43,4 +43,16 @@ def get_weather(city):
     background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
     C.pack()
+frame = tk.Frame(app,  bg='#42c2f4', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+#frame_window = C.create_window(100, 40, window=frame)
 
+textbox = tk.Entry(frame, font=40)
+textbox.place(relwidth=0.65, relheight=1)
+
+submit = tk.Button(frame, text='Get Weather', font=40, command=lambda: get_weather(textbox.get()))
+#submit.config(font=)
+submit.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(app, bg='#42c2f4', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
