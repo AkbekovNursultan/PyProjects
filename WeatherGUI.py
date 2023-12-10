d9d668fa5ecca81ce5a41d6b9bf47eb0ea8532ee
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-12-10 16:48:51 +0600

    add new Background, small adjustments

--- WeatherGUI.py
+++ WeatherGUI.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+from tkinter import font
 import requests
 from PIL import Image, ImageTk
+
+
 
 app = tk.Tk()
 
 HEIGHT = 500
 WIDTH = 600
+app.resizable(height = 0, width = 0)
 
 def format_response(weather_json):
     try:
@@ -17,10 +21,12 @@ def format_response(weather_json):
         final_str = 'There was a problem retrieving that information'
     #final_str = 'hello'
     return final_str
+
+
 def get_weather(city):
     weather_key = 'edffd1bf975a74d5d10e58c5ac8be2d3'
     url = 'https://api.openweathermap.org/data/2.5/weather'
-    params = {'APPID': 'edffd1bf975a74d5d10e58c5ac8be2d3', 'q': city, 'units':'metric'}
+    params = {'APPID': weather_key, 'q': city, 'units':'metric'}
     response = requests.get(url, params=params)
     print(response.json())
     weather_json = response.json()
@@ -34,20 +40,21 @@ def open_image(icon):
     size = int(lower_frame.winfo_height()*0.25)
     img = ImageTk.PhotoImage(Image.open('./img/'+icon+'.png').resize((size, size)))
     weather_icon.delete("all")
-    weather_icon.create_image(0,0, anchor='nw', image=img)
+    weather_icon.create_image(0, 0, anchor='nw', image=img)
     weather_icon.image = img
 
 C = tk.Canvas(app, height=HEIGHT, width=WIDTH)
-background_image= tk.PhotoImage(file='./landscape.png')
+background_image= tk.PhotoImage(file='./background.png')
 background_label = tk.Label(app, image=background_image)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 C.pack()
+
 frame = tk.Frame(app,  bg='#42c2f4', bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 #frame_window = C.create_window(100, 40, window=frame)
 
-textbox = tk.Entry(frame, font=40)
+textbox = tk.Entry(frame, font=('Comic sans MS', 20))
 textbox.place(relwidth=0.65, relheight=1)
 
 submit = tk.Button(frame, text='Get Weather', font=40, command=lambda: get_weather(textbox.get()))
@@ -58,8 +65,8 @@ lower_frame = tk.Frame(app, bg='#42c2f4', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
 bg_color = 'white'
-results = tk.Label(lower_frame, anchor='nw', justify='left', bd=4)
-results.config(font=40, bg=bg_color)
+results = tk.Label(lower_frame, font=('Comic sans MS', 15), anchor='nw', justify='left', bd=4)
+results.config(font=('Comic sans MS', 15), bg=bg_color)
 results.place(relwidth=1, relheight=1)
 
 weather_icon = tk.Canvas(results, bg=bg_color, bd=0, highlightthickness=0)
